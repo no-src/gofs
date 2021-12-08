030f04fad5fb439b02531233f07b079c6fe938b5
mstmdev <mstmdev@gmail.com>
2021-12-08 13:39:31 +0800

    Change the log_file and log_flush flags to true as default

--- cmd/gofs/flag.go
+++ cmd/gofs/flag.go
@@ -86,9 +86,9 @@ func parseFlags() {
 
 	// log
 	flag.IntVar(&logLevel, "log_level", int(log.InfoLevel), "set log level, default is INFO. DEBUG=0 INFO=1 WARN=2 ERROR=3")
-	flag.BoolVar(&enableFileLogger, "log_file", false, "enable the file logger")
+	flag.BoolVar(&enableFileLogger, "log_file", true, "enable the file logger")
 	flag.StringVar(&logDir, "log_dir", "./logs/", "set the directory of log file")
-	flag.BoolVar(&logFlush, "log_flush", false, "enable auto flush log with interval")
+	flag.BoolVar(&logFlush, "log_flush", true, "enable auto flush log with interval")
 	flag.DurationVar(&logFlushInterval, "log_flush_interval", time.Second*3, "set the log flush interval duration, you need to enable -log_flush first")
 
 	// daemon
