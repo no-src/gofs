05559a186ba3ee4d5e30b0c6b1d0b6ee495fd6ea
mstmdev <mstmdev@gmail.com>
2022-02-15 18:13:32 +0800

    Add the PushAddr field for FileServerInfo

--- contract/file_server_info.go
+++ contract/file_server_info.go
@@ -9,6 +9,8 @@ type FileServerInfo struct {
 	SourcePath string `json:"source_path"`
 	// DestPath the dest base path of the file server
 	DestPath string `json:"dest_path"`
-	// QueryAddr the query api path of the file server
+	// QueryAddr the query api address of the file server
 	QueryAddr string `json:"query_addr"`
+	// PushAddr the push api address of the file server
+	PushAddr string `json:"push_addr"`
 }
