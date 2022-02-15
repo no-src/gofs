c28f80789d8fb77a3feef4124c1b6b38a7f2baa1
mstmdev <mstmdev@gmail.com>
2022-02-15 18:11:50 +0800

    Add the HttpPostWithCookie function, send a post request with form data and cookies Add the HttpPostFileWithCookie function, send a post request with form data, file and cookies

--- util/http.go
+++ util/http.go
@@ -1,10 +1,15 @@
 package util
 
 import (
+	"bytes"
 	"crypto/tls"
+	"io"
+	"mime/multipart"
 	"net"
 	"net/http"
 	"net/url"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -32,6 +37,62 @@ func HttpGetWithCookie(url string, cookies ...*http.Cookie) (resp *http.Response
 // HttpPost send a post request with form data
 func HttpPost(url string, data url.Values) (resp *http.Response, err error) {
 	return defaultClient.PostForm(url, data)
+}
+
+// HttpPostWithCookie send a post request with form data and cookies
+func HttpPostWithCookie(url string, data url.Values, cookies ...*http.Cookie) (resp *http.Response, err error) {
+	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(data.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	for _, cookie := range cookies {
+		req.AddCookie(cookie)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	return defaultClient.Do(req)
+}
+
+// HttpPostFileWithCookie send a post request with form data, file and cookies
+func HttpPostFileWithCookie(url string, fieldName, fileName string, data url.Values, cookies ...*http.Cookie) (resp *http.Response, err error) {
+	f, err := os.Open(fieldName)
+	if err != nil {
+		return nil, err
+	}
+	body := new(bytes.Buffer)
+	w := multipart.NewWriter(body)
+
+	for k, v := range data {
+		for _, item := range v {
+			w.WriteField(k, item)
+		}
+	}
+
+	fw, err := w.CreateFormFile(fieldName, fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = io.Copy(fw, f)
+	if err != nil {
+		return nil, err
+	}
+	err = w.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(cookies) > 0 {
+		for _, cookie := range cookies {
+			req.AddCookie(cookie)
+		}
+	}
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	return defaultClient.Do(req)
 }
 
 // HttpPostWithoutRedirect send a post request with form data and not auto redirect
