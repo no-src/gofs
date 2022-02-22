c38905e6224b6b403fcada59c6a2c93cf0436c8d
mstmdev <mstmdev@gmail.com>
2022-02-22 14:53:08 +0800

    Add test for ignore

--- ignore/ignore_test.go
+++ ignore/ignore_test.go
@@ -3,6 +3,7 @@ package ignore
 import "testing"
 
 func TestMatch(t *testing.T) {
+	resetDefaultIgnore()
 	err := Init("./testdata/demo.ignore", true)
 	if err != nil {
 		t.Errorf("init default ignore component error => %v", err)
@@ -29,4 +30,71 @@ func testMatch(t *testing.T, expect bool, path string) {
 		t.Logf("[%s] => expect: %v, but actual: %v \n", path, expect, actual)
 		t.Fail()
 	}
+}
+
+func TestMatchPath(t *testing.T) {
+	resetDefaultIgnore()
+	err := Init("./testdata/demo.ignore", true)
+	if err != nil {
+		t.Errorf("init default ignore component error => %v", err)
+		t.FailNow()
+		return
+	}
+	testMatchPath(t, true, "/hello.txt.1643351810.deleted")
+	testMatchPath(t, false, "/hello.txt")
+
+	err = Init("./testdata/demo.ignore", false)
+	if err != nil {
+		t.Errorf("init default ignore component error => %v", err)
+		t.FailNow()
+		return
+	}
+	testMatchPath(t, false, "/hello.txt.1643351810.deleted")
+	testMatchPath(t, false, "/hello.txt")
+	testMatchPath(t, true, "/source/bin/")
+}
+
+func testMatchPath(t *testing.T, expect bool, path string) {
+	actual := MatchPath(path, "test suit", "test")
+	if actual != expect {
+		t.Logf("[%s] => expect: %v, but actual: %v \n", path, expect, actual)
+		t.Fail()
+	}
+}
+
+func TestParseError(t *testing.T) {
+	text := "/error**"
+	_, err := parse([]byte(text))
+	if err == nil {
+		t.Errorf("parse the rule text should be return error => [%s] error => %v", text, err)
+		t.FailNow()
+		return
+	}
+}
+
+func TestInitErrorFileNotFound(t *testing.T) {
+	resetDefaultIgnore()
+	c := "./testdata/notfound.ignore"
+	err := Init(c, true)
+	if err == nil {
+		t.Errorf("init default ignore component should be return error => %s", c)
+		t.FailNow()
+		return
+	}
+}
+
+func TestInitWithNoConfig(t *testing.T) {
+	resetDefaultIgnore()
+	err := Init("", true)
+	if err != nil {
+		t.Errorf("init default ignore component error => %v", err)
+		t.FailNow()
+		return
+	}
+	testMatch(t, false, "bin")
+}
+
+func resetDefaultIgnore() {
+	defaultIgnore = nil
+	matchIgnoreDeletedPath = false
 }
