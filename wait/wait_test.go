dfa2b4359224777fbca52d90b0bcae13d74ee0d7
mstmdev <mstmdev@gmail.com>
2022-03-11 14:09:04 +0800

    Add tests for the wait package

---
+++ wait/wait_test.go
@@ -0,0 +1,32 @@
+package wait
+
+import (
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestWaitDone(t *testing.T) {
+	wd := NewWaitDone()
+	go func() {
+		time.After(time.Millisecond)
+		wd.Done()
+	}()
+	err := wd.Wait()
+	if err != nil {
+		t.Errorf("test wait done error =>%s", err)
+	}
+}
+
+func TestWaitDoneWithError(t *testing.T) {
+	errWaitDone := errors.New("wait done error")
+	wd := NewWaitDone()
+	go func() {
+		time.After(time.Millisecond)
+		wd.DoneWithError(errWaitDone)
+	}()
+	err := wd.Wait()
+	if err == nil || !errors.Is(err, errWaitDone) {
+		t.Errorf("test wait done with error failed, expect:%s actual:%s", errWaitDone, err)
+	}
+}
