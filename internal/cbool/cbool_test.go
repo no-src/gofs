5083656af409dcbf73f4c3a09e52ec55be49b574
mstmdev <mstmdev@gmail.com>
2022-03-15 11:25:18 +0800

    Add tests for the internal/cbool package

---
+++ internal/cbool/cbool_test.go
@@ -0,0 +1,30 @@
+package cbool
+
+import "testing"
+
+func TestCBool(t *testing.T) {
+	expect := true
+	cb := New(expect)
+	actual := cb.Get()
+	if actual != expect {
+		t.Errorf("test CBoll New and Get failed, expect:%v, actual:%v", expect, actual)
+	}
+
+	expect = false
+	cb.Set(expect)
+	actual = cb.Get()
+	if actual != expect {
+		t.Errorf("test CBoll Set and Get failed, expect:%v, actual:%v", expect, actual)
+	}
+
+	expect = true
+	c := cb.SetC(expect)
+	actual = cb.Get()
+	if actual != expect {
+		t.Errorf("test CBoll SetC and Get failed, expect:%v, actual:%v", expect, actual)
+	}
+	actual = <-c
+	if actual != expect {
+		t.Errorf("test CBoll SetC value failed, expect:%v, actual:%v", expect, actual)
+	}
+}
