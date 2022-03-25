8a7bc84b988b28f511e79e36187b84999b20dd53
mstmdev <mstmdev@gmail.com>
2022-03-26 04:20:16 +0800

    Add the TopList struct and tests, store some elements in list with specified capacity, the oldest elements that exceed specified capacity will be discarded

---
+++ internal/toplist/toplist.go
@@ -0,0 +1,139 @@
+package toplist
+
+import (
+	"errors"
+	"sync"
+)
+
+// TopList store some elements in list with specified capacity, the oldest elements that exceed specified capacity will be discarded
+type TopList struct {
+	capacity int
+	length   int
+	begin    int
+	end      int
+	data     []interface{}
+	mu       sync.RWMutex
+	asc      bool
+}
+
+var errInvalidCapacity = errors.New("capacity must be greater than zero")
+
+// New create a TopList with specified capacity and the capacity must be greater than zero, order by add time desc
+func New(capacity int) (*TopList, error) {
+	return newTopList(capacity, false)
+}
+
+// NewOrderByAsc create a TopList with specified capacity and the capacity must be greater than zero, order by add time asc
+func NewOrderByAsc(capacity int) (*TopList, error) {
+	return newTopList(capacity, true)
+}
+
+func newTopList(capacity int, asc bool) (*TopList, error) {
+	if capacity <= 0 {
+		return nil, errInvalidCapacity
+	}
+	tl := &TopList{
+		capacity: capacity,
+		begin:    0,
+		end:      -1,
+		data:     make([]interface{}, capacity),
+		asc:      asc,
+	}
+	return tl, nil
+}
+
+// Add add an element to the list
+func (tl *TopList) Add(element interface{}) {
+	tl.mu.Lock()
+	defer tl.mu.Unlock()
+	nextEnd := tl.getNextEnd()
+	tl.data[nextEnd] = element
+	tl.end = nextEnd
+	if tl.length < tl.capacity {
+		tl.length++
+	} else {
+		tl.begin = tl.getNextBegin()
+	}
+}
+
+// Len returns the length of the list
+func (tl *TopList) Len() int {
+	tl.mu.RLock()
+	defer tl.mu.RUnlock()
+	return tl.length
+}
+
+// Cap returns the capacity of the list
+func (tl *TopList) Cap() int {
+	tl.mu.RLock()
+	defer tl.mu.RUnlock()
+	return tl.capacity
+}
+
+// Get get an element by a specified index in the list, if the index is greater than TopList.Len or less than 0, return a nil element always
+func (tl *TopList) Get(index int) interface{} {
+	tl.mu.RLock()
+	defer tl.mu.RUnlock()
+	return tl.get(index)
+}
+
+func (tl *TopList) get(index int) interface{} {
+	if index >= tl.length || index < 0 {
+		return nil
+	}
+	if !tl.asc {
+		index = tl.length - 1 - index
+	}
+
+	pos := tl.begin + index
+	if pos >= tl.capacity {
+		pos -= tl.capacity
+	}
+	return tl.data[pos]
+}
+
+// Last returns the last element of the list
+func (tl *TopList) Last() interface{} {
+	tl.mu.RLock()
+	defer tl.mu.RUnlock()
+	if tl.length <= 0 {
+		return nil
+	}
+	if tl.asc {
+		return tl.data[tl.end]
+	}
+	return tl.data[tl.begin]
+}
+
+// Top returns the latest elements by top n
+func (tl *TopList) Top(n int) (list []interface{}) {
+	tl.mu.RLock()
+	defer tl.mu.RUnlock()
+	if n <= 0 || tl.length == 0 {
+		return list
+	}
+
+	if tl.length < n {
+		n = tl.length
+	}
+	for i := 0; i < n; i++ {
+		list = append(list, tl.get(i))
+	}
+	return list
+}
+
+func (tl *TopList) getNextPos(pos int) int {
+	next := pos + 1
+	if next >= tl.capacity {
+		next = 0
+	}
+	return next
+}
+
+func (tl *TopList) getNextBegin() int {
+	return tl.getNextPos(tl.begin)
+}
+
+func (tl *TopList) getNextEnd() int {
+	return tl.getNextPos(tl.end)
+}
---
+++ internal/toplist/toplist_test.go
@@ -0,0 +1,217 @@
+package toplist
+
+import "testing"
+
+func TestTopListWithCapZero(t *testing.T) {
+	// desc
+	capacity := 0
+	_, err := New(capacity)
+	if err != errInvalidCapacity {
+		t.Errorf("[desc] test toplist with zero capacity error, expect get an error")
+	}
+
+	// asc
+	capacity = 0
+	_, err = NewOrderByAsc(capacity)
+	if err != errInvalidCapacity {
+		t.Errorf("[asc] test toplist with zero capacity error, expect get an error")
+	}
+}
+
+func TestTopListWithCapOne(t *testing.T) {
+	// desc
+	capacity := 1
+	tl, err := New(capacity)
+	if err != nil {
+		t.Errorf("[desc] test toplist with one capacity error, get an error =>%v", err)
+		return
+	}
+
+	topListLast(t, tl, nil)
+
+	s := "hello"
+	tl.Add(s)
+	topListGet(t, tl, 0, s)
+
+	s = "world"
+	tl.Add(s)
+	topListGet(t, tl, 0, s)
+	topListGet(t, tl, -1, nil)
+	topListGet(t, tl, capacity, nil)
+	topListLast(t, tl, s)
+	topListLen(t, tl, capacity)
+	topListCap(t, tl, capacity)
+	topListTop(t, tl, 0)
+	topListTop(t, tl, 1, s)
+	topListTop(t, tl, 2, s)
+
+	// asc
+	capacity = 1
+	tl, err = NewOrderByAsc(capacity)
+	if err != nil {
+		t.Errorf("[asc] test toplist with one capacity error, get an error =>%v", err)
+		return
+	}
+
+	topListLast(t, tl, nil)
+
+	s = "hello"
+	tl.Add(s)
+	topListGet(t, tl, 0, s)
+
+	s = "world"
+	tl.Add(s)
+	topListGet(t, tl, 0, s)
+	topListGet(t, tl, -1, nil)
+	topListGet(t, tl, capacity, nil)
+	topListLast(t, tl, s)
+	topListLen(t, tl, capacity)
+	topListCap(t, tl, capacity)
+	topListTop(t, tl, 0)
+	topListTop(t, tl, 1, s)
+	topListTop(t, tl, 2, s)
+}
+
+func TestTopListWithCapTwo(t *testing.T) {
+	// desc
+	capacity := 2
+	tl, err := New(capacity)
+	if err != nil {
+		t.Errorf("[desc] test toplist with one capacity error, get an error =>%v", err)
+		return
+	}
+
+	topListLast(t, tl, nil)
+
+	s1 := "hello"
+	tl.Add(s1)
+	topListGet(t, tl, 0, s1)
+	topListLen(t, tl, 1)
+
+	s2 := "world"
+	tl.Add(s2)
+	topListGet(t, tl, 0, s2)
+	topListGet(t, tl, 1, s1)
+	topListGet(t, tl, -1, nil)
+	topListGet(t, tl, capacity, nil)
+	topListLast(t, tl, s1)
+	topListLen(t, tl, capacity)
+	topListCap(t, tl, capacity)
+
+	s3 := "golang"
+	tl.Add(s3)
+	topListGet(t, tl, 0, s3)
+
+	topListGet(t, tl, 1, s2)
+	topListGet(t, tl, -1, nil)
+	topListGet(t, tl, capacity, nil)
+	topListLast(t, tl, s2)
+	topListLen(t, tl, capacity)
+	topListCap(t, tl, capacity)
+	topListTop(t, tl, 0)
+	topListTop(t, tl, 1, s3)
+	topListTop(t, tl, 2, s3, s2)
+
+	// asc
+	capacity = 2
+	tl, err = NewOrderByAsc(capacity)
+	if err != nil {
+		t.Errorf("[asc] test toplist with one capacity error, get an error =>%v", err)
+		return
+	}
+
+	topListLast(t, tl, nil)
+
+	s1 = "hello"
+	tl.Add(s1)
+	topListGet(t, tl, 0, s1)
+	topListLen(t, tl, 1)
+
+	s2 = "world"
+	tl.Add(s2)
+	topListGet(t, tl, 0, s1)
+	topListGet(t, tl, 1, s2)
+	topListGet(t, tl, -1, nil)
+	topListGet(t, tl, capacity, nil)
+	topListLast(t, tl, s2)
+	topListLen(t, tl, capacity)
+	topListCap(t, tl, capacity)
+
+	s3 = "golang"
+	tl.Add(s3)
+	topListGet(t, tl, 0, s2)
+
+	topListGet(t, tl, 1, s3)
+	topListGet(t, tl, -1, nil)
+	topListGet(t, tl, capacity, nil)
+	topListLast(t, tl, s3)
+	topListLen(t, tl, capacity)
+	topListCap(t, tl, capacity)
+	topListTop(t, tl, 0)
+	topListTop(t, tl, 1, s2)
+	topListTop(t, tl, 2, s2, s3)
+}
+
+func topListGet(t *testing.T, tl *TopList, index int, expect interface{}) {
+	order := "desc"
+	if tl.asc {
+		order = "asc"
+	}
+	actual := tl.Get(index)
+	if actual != expect {
+		t.Errorf("[%s] get toplist element that index=0 error, expect:%v, actual:%v", order, expect, actual)
+	}
+}
+
+func topListLast(t *testing.T, tl *TopList, expect interface{}) {
+	order := "desc"
+	if tl.asc {
+		order = "asc"
+	}
+	actual := tl.Last()
+	if actual != expect {
+		t.Errorf("[%s] get toplist last element error, expect:%v, actual:%s", order, expect, actual)
+	}
+}
+
+func topListLen(t *testing.T, tl *TopList, expect int) {
+	order := "desc"
+	if tl.asc {
+		order = "asc"
+	}
+	actual := tl.Len()
+	if actual != expect {
+		t.Errorf("[%s] get toplist length error, expect:%d, actual:%d", order, expect, actual)
+	}
+}
+
+func topListCap(t *testing.T, tl *TopList, expect int) {
+	order := "desc"
+	if tl.asc {
+		order = "asc"
+	}
+	actual := tl.Cap()
+	if actual != expect {
+		t.Errorf("[%s] get toplist capacity error, expect:%d, actual:%d", order, expect, actual)
+	}
+}
+
+func topListTop(t *testing.T, tl *TopList, top int, expect ...interface{}) {
+	order := "desc"
+	if tl.asc {
+		order = "asc"
+	}
+	actual := tl.Top(top)
+	if len(actual) != len(expect) {
+		t.Errorf("[%s] get toplist top error, expect length:%d, actual length:%d", order, len(expect), len(actual))
+		return
+	}
+
+	for index, expectElement := range expect {
+		actualElement := actual[index]
+		if actualElement != expectElement {
+			t.Errorf("[%s] get toplist top error, expect element:%v, actual element:%v", order, expectElement, actualElement)
+			return
+		}
+	}
+}
