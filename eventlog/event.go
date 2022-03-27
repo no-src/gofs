f4d0354f9f6d4e925a1d94754a6d3c55d4337a04
mstmdev <mstmdev@gmail.com>
2022-03-28 01:58:49 +0800

    Add json tags for the fields of Event

--- eventlog/event.go
+++ eventlog/event.go
@@ -3,14 +3,16 @@ package eventlog
 import (
 	"fmt"
 	"github.com/no-src/gofs/util/timeutil"
-	"time"
 )
 
 // Event the description of file change event
 type Event struct {
-	Name string
-	Op   string
-	Time timeutil.Time
+	// Name the path of file change
+	Name string `json:"name"`
+	// Op the operation of file change
+	Op string `json:"op"`
+	// Time the time of file change
+	Time timeutil.Time `json:"time"`
 }
 
 // NewEvent create an event instance
@@ -18,7 +20,7 @@ func NewEvent(name, op string) Event {
 	return Event{
 		Name: name,
 		Op:   op,
-		Time: timeutil.NewTime(time.Now()),
+		Time: timeutil.Now(),
 	}
 }
 
