cfdffac6b5d8bbb116f5ab63ce8a2f189166b628
mstmdev <mstmdev@gmail.com>
2022-04-28 00:12:15 +0800

    Update build-docker.sh

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -4,7 +4,7 @@
 # git pull --no-rebase
 
 # update the last git commit
-echo $(git rev-parse main) >version/commit
+echo -e "$(git rev-parse main)\c" >version/commit
 
 # set GOPROXY environment variable
 # export GOPROXY=https://goproxy.cn
@@ -21,11 +21,17 @@ export GOFSIMAGETAG=latest
 # build gofs
 docker run --rm -v "$PWD":/usr/src/gofs -w /usr/src/gofs -e GOPROXY=$GOPROXY golang:$GOIMAGETAG go build -v -tags netgo -o . ./...
 
+# reset commit file
+echo -e "\c" >version/commit
+
 # remove the existing old image
-docker rmi -f gofs
+docker rmi -f $GOFSIMAGENAME
 
 # build Dockerfile
 docker build -t $GOFSIMAGENAME:$GOFSIMAGETAG .
 
 # run a container to print the gofs version
 docker run -it --rm --name running-gofs-version $GOFSIMAGENAME:$GOFSIMAGETAG gofs -v
+
+# clear gofs binary file
+rm -rf gofs
