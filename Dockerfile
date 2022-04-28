c9ddf97f413024c0390b4f522acff847094797b9
mstmdev <mstmdev@gmail.com>
2022-04-29 03:12:13 +0800

    Update Dockerfile and build script, use multi-stage builds

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,23 @@
+FROM golang:latest AS build
+
+WORKDIR /gowork
+
+ARG GOPROXY
+
+COPY . .
+
+RUN echo $(git rev-parse main) >version/commit
+
+RUN go build -v -tags netgo -o . ./...
+
 FROM alpine:3.15.4
 
 WORKDIR /app
 
 ENV PATH=$PATH:/app
 
-EXPOSE 80
-EXPOSE 443
-EXPOSE 8105
+EXPOSE 80 443 8105
 
-COPY ./gofs .
+COPY --from=build /gowork/gofs .
 
 WORKDIR /workspace
--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -3,35 +3,17 @@
 # update git repository
 # git pull --no-rebase
 
-# update the last git commit
-echo -e "$(git rev-parse main)\c" >version/commit
-
-# set GOPROXY environment variable
-# export GOPROXY=https://goproxy.cn
-
-# set the golang version by GOIMAGETAG environment variable
-export GOIMAGETAG=latest
-
 # set the gofs docker image name by GOFSIMAGENAME environment variable
 export GOFSIMAGENAME=nosrc/gofs
 
 # set the gofs docker image tag by GOFSIMAGETAG environment variable
 export GOFSIMAGETAG=latest
 
-# build gofs
-docker run --rm -v "$PWD":/usr/src/gofs -w /usr/src/gofs -e GOPROXY=$GOPROXY golang:$GOIMAGETAG go build -v -tags netgo -o . ./...
-
-# reset commit file
-echo -e "\c" >version/commit
-
 # remove the existing old image
 docker rmi -f $GOFSIMAGENAME
 
 # build Dockerfile
-docker build -t $GOFSIMAGENAME:$GOFSIMAGETAG .
+docker build --build-arg GOPROXY=$GOPROXY -t $GOFSIMAGENAME:$GOFSIMAGETAG .
 
 # run a container to print the gofs version
 docker run -it --rm --name running-gofs-version $GOFSIMAGENAME:$GOFSIMAGETAG gofs -v
-
-# clear gofs binary file
-rm -rf gofs
