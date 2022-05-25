fa1187d21ebd8ea2165837cbc61740abf2ca8b30
mstmdev <mstmdev@gmail.com>
2022-05-26 00:39:44 +0800

    Add more optional hash algorithms and tests

--- util/hashutil/factory.go
+++ util/hashutil/factory.go
@@ -2,7 +2,14 @@ package hashutil
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"hash"
+	"hash/adler32"
+	"hash/crc32"
+	"hash/crc64"
+	"hash/fnv"
 )
 
 var (
@@ -18,6 +25,54 @@ func InitDefaultHash(algorithm string) {
 		factory = func() hash.Hash {
 			return md5.New()
 		}
+	case SHA1Hash:
+		factory = func() hash.Hash {
+			return sha1.New()
+		}
+	case SHA256Hash:
+		factory = func() hash.Hash {
+			return sha256.New()
+		}
+	case SHA512Hash:
+		factory = func() hash.Hash {
+			return sha512.New()
+		}
+	case CRC32Hash:
+		factory = func() hash.Hash {
+			return crc32.NewIEEE()
+		}
+	case CRC64Hash:
+		factory = func() hash.Hash {
+			return crc64.New(crc64.MakeTable(crc64.ISO))
+		}
+	case Adler32Hash:
+		factory = func() hash.Hash {
+			return adler32.New()
+		}
+	case FNV132Hash:
+		factory = func() hash.Hash {
+			return fnv.New32()
+		}
+	case FNV1A32Hash:
+		factory = func() hash.Hash {
+			return fnv.New32a()
+		}
+	case FNV164Hash:
+		factory = func() hash.Hash {
+			return fnv.New64()
+		}
+	case FNV1A64Hash:
+		factory = func() hash.Hash {
+			return fnv.New64a()
+		}
+	case FNV1128Hash:
+		factory = func() hash.Hash {
+			return fnv.New128()
+		}
+	case FNV1A128Hash:
+		factory = func() hash.Hash {
+			return fnv.New128a()
+		}
 	}
 }
 
@@ -27,10 +82,36 @@ func New() hash.Hash {
 }
 
 const (
+	// DefaultHash the default hash algorithm
+	DefaultHash = MD5Hash
 	// MD5Hash the MD5 hash algorithm
 	MD5Hash = "md5"
+	// SHA1Hash the SHA-1 hash algorithm
+	SHA1Hash = "sha1"
+	// SHA256Hash the SHA256 hash algorithm
+	SHA256Hash = "sha256"
+	// SHA512Hash the SHA-512 hash algorithm
+	SHA512Hash = "sha512"
+	// CRC32Hash the CRC-32 checksum
+	CRC32Hash = "crc32"
+	// CRC64Hash the CRC-64 checksum
+	CRC64Hash = "crc64"
+	// Adler32Hash the Adler-32 checksum
+	Adler32Hash = "adler32"
+	// FNV132Hash the 32-bit FNV-1 non-cryptographic hash function
+	FNV132Hash = "fnv-1-32"
+	// FNV1A32Hash the 32-bit FNV-1a non-cryptographic hash function
+	FNV1A32Hash = "fnv-1a-32"
+	// FNV164Hash the 64-bit FNV-1 non-cryptographic hash function
+	FNV164Hash = "fnv-1-64"
+	// FNV1A64Hash the 64-bit FNV-1a non-cryptographic hash function
+	FNV1A64Hash = "fnv-1a-64"
+	// FNV1128Hash the 128-bit FNV-1 non-cryptographic hash function
+	FNV1128Hash = "fnv-1-128"
+	// FNV1A128Hash the 128-bit FNV-1a non-cryptographic hash function
+	FNV1A128Hash = "fnv-1a-128"
 )
 
 func init() {
-	InitDefaultHash(MD5Hash)
+	InitDefaultHash(DefaultHash)
 }
---
+++ util/hashutil/factory_test.go
@@ -0,0 +1,40 @@
+package hashutil
+
+import (
+	"testing"
+)
+
+func TestAllHashAlgorithms(t *testing.T) {
+	input := "hello gopher"
+	testCases := []struct {
+		algorithm string
+		input     string
+		expect    string
+	}{
+		{MD5Hash, input, "0e181d273c0a4593230ce8319eaae9b7"},
+		{SHA1Hash, input, "07adeacfcdde2146d26119d79cb0028d831e46e9"},
+		{SHA256Hash, input, "f7392c4f40eb32d21e6dc087a00049e914f6ce69b76271f46f2b91c53f35166c"},
+		{SHA512Hash, input, "d65b9e26ebef94e8984eeb5bba2558183db4a312ca750495e427b288739eebc67ce732b234210638dabe914a9f7202b2facd248ddb753fc649de83574aa2d231"},
+		{CRC32Hash, input, "461b91ae"},
+		{CRC64Hash, input, "086879fc3731d9ac"},
+		{Adler32Hash, input, "1e6804ba"},
+		{FNV132Hash, input, "5e358952"},
+		{FNV1A32Hash, input, "6836946c"},
+		{FNV164Hash, input, "0039da00cd5a26d2"},
+		{FNV1A64Hash, input, "d9ced1ff0e72c2cc"},
+		{FNV1128Hash, input, "af0bcdfc672247e14fc2ae047bc55b02"},
+		{FNV1A128Hash, input, "1a83a6c3193dcc0fbcc90891f7d3df14"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.algorithm, func(t *testing.T) {
+			InitDefaultHash(tc.algorithm)
+			if actual := Hash([]byte(input)); actual != tc.expect {
+				t.Errorf("calculate hash with %s algorithm error , expect:%s, but actual:%s", tc.algorithm, tc.expect, actual)
+			}
+		})
+	}
+
+	// reset default hash algorithm
+	InitDefaultHash(DefaultHash)
+}
