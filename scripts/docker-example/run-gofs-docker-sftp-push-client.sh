cc2bc4d06a7982bb71c2e2a70a7494351d15c920
mstmdev <mstmdev@gmail.com>
2022-08-01 00:48:12 +0800

    Update the script scripts/docker-example/run-gofs-docker-sftp-push-client.sh

--- scripts/docker-example/run-gofs-docker-sftp-push-client.sh
+++ scripts/docker-example/run-gofs-docker-sftp-push-client.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p sftp-client/source sftp-client/dest
+mkdir -p sftp-push-client/source sftp-push-client/dest
 
 # depending on your situation, set the SFTP server address
 export SFTP_SERVER_ADDR=10.0.4.8
@@ -8,5 +8,5 @@ export SFTP_SERVER_REMOTE_PATH=/gofs_sftp_server
 
 export WORKDIR=/workspace
 
-docker run -it --rm -v "$PWD":"$WORKDIR" --name running-gofs-sftp-client nosrc/gofs:latest \
-  gofs -source="$WORKDIR/sftp-client/source" -dest="sftp://$SFTP_SERVER_ADDR:22?local_sync_disabled=false&path=$WORKDIR/sftp-client/dest&remote_path=$SFTP_SERVER_REMOTE_PATH" -users="sftp_user|sftp_pwd"
+docker run -it --rm -v "$PWD":"$WORKDIR" --name running-gofs-sftp-push-client nosrc/gofs:latest \
+  gofs -source="$WORKDIR/sftp-push-client/source" -dest="sftp://$SFTP_SERVER_ADDR:22?local_sync_disabled=false&path=$WORKDIR/sftp-push-client/dest&remote_path=$SFTP_SERVER_REMOTE_PATH" -users="sftp_user|sftp_pwd"
