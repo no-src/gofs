ed16a22e4c0f94db4fb15fa30d4915a2b32e499f
mstmdev <mstmdev@gmail.com>
2022-08-02 00:20:23 +0800

    Add the script scripts/docker-example/run-gofs-docker-sftp-pull-client.sh

---
+++ scripts/docker-example/run-gofs-docker-sftp-pull-client.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+mkdir -p sftp-pull-client/source sftp-pull-client/dest
+
+# depending on your situation, set the SFTP server address
+export SFTP_SERVER_ADDR=10.0.4.8
+export SFTP_SERVER_REMOTE_PATH=/gofs_sftp_server
+
+export WORKDIR=/workspace
+
+docker run -it --rm -v "$PWD":"$WORKDIR" --name running-gofs-sftp-pull-client nosrc/gofs:latest \
+  gofs -source="sftp://$SFTP_SERVER_ADDR:22?remote_path=$SFTP_SERVER_REMOTE_PATH" -dest="$WORKDIR/sftp-pull-client/dest" -users="sftp_user|sftp_pwd" -sync_once
