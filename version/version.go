9829169026e5f25865c544226383161980b1b7ff
mstmdev <mstmdev@gmail.com>
2022-11-19 06:40:51 +0800

    Execute go fmt sync.go

--- version/version.go
+++ version/version.go
@@ -9,7 +9,7 @@ import (
 )
 
 // VERSION the current program version info
-const VERSION = "v0.5.0"
+const VERSION = "v0.5.1"
 
 // PrintVersion print the current version info, and append the commit info if the commit file is not empty
 func PrintVersion() {
