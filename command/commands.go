26204061527abd0a321072b5ff5a3f13c77d51f4
mstmdev <mstmdev@gmail.com>
2022-12-01 10:25:39 +0800

    Add error log

--- command/commands.go
+++ command/commands.go
@@ -1,5 +1,7 @@
 package command
 
+import "github.com/no-src/log"
+
 // Commands the commands that contain the init, actions and clear stages
 type Commands struct {
 	Name    string
@@ -21,22 +23,23 @@ func (cs *Commands) Exec() (err error) {
 
 // ExecInit execute the init commands
 func (cs *Commands) ExecInit() (err error) {
-	return cs.exec(cs.Init)
+	return cs.exec("init", cs.Init)
 }
 
 // ExecActions execute the actions commands
 func (cs *Commands) ExecActions() (err error) {
-	return cs.exec(cs.Actions)
+	return cs.exec("actions", cs.Actions)
 }
 
 // ExecClear execute the clear commands
 func (cs *Commands) ExecClear() (err error) {
-	return cs.exec(cs.Clear)
+	return cs.exec("clear", cs.Clear)
 }
 
-func (cs *Commands) exec(commands []Command) (err error) {
-	for _, c := range commands {
+func (cs *Commands) exec(stage string, commands []Command) (err error) {
+	for i, c := range commands {
 		if err = c.Exec(); err != nil {
+			log.Error(err, "[%s] [%d] execute failed", stage, i+1)
 			return err
 		}
 	}
