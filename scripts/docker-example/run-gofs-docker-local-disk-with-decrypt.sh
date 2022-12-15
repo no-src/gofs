420be54abee0ffa5a3efa64b9d1fda187d07ad94
mstmdev <mstmdev@gmail.com>
2022-12-15 09:16:48 +0800

    Update run-gofs-docker-local-disk-with-decrypt.sh script, remove unnecessary parameters

--- scripts/docker-example/run-gofs-docker-local-disk-with-decrypt.sh
+++ scripts/docker-example/run-gofs-docker-local-disk-with-decrypt.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p source/encrypt dest/encrypt decrypt_out
+mkdir -p dest/encrypt decrypt_out
 
 export WORKDIR=/workspace
 
 docker run -it --rm -v "$PWD":"$WORKDIR" --name running-gofs-local-disk-with-decrypt nosrc/gofs:latest \
-  gofs -source="$WORKDIR/source" -dest="$WORKDIR/dest" -decrypt -decrypt_path="$WORKDIR/dest/encrypt" -decrypt_secret=helloworld -decrypt_out="$WORKDIR/decrypt_out"
+  gofs -decrypt -decrypt_path="$WORKDIR/dest/encrypt" -decrypt_secret=helloworld -decrypt_out="$WORKDIR/decrypt_out"
