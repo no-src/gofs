cedc0661d612ebf0cf1f9d8be5d3de56d1071c23
mstmdev <mstmdev@gmail.com>
2023-01-30 00:20:25 +0800

    The WaitDone supports calling the Wait function many times (#82)

--- wait/wait_test.go
+++ wait/wait_test.go
@@ -2,8 +2,13 @@ package wait
 
 import (
 	"errors"
+	"sync"
 	"testing"
 	"time"
+)
+
+var (
+	errWaitDone = errors.New("wait done error")
 )
 
 func TestWaitDone(t *testing.T) {
@@ -19,7 +24,7 @@ func TestWaitDone(t *testing.T) {
 }
 
 func TestWaitDone_ReturnError(t *testing.T) {
-	errWaitDone := errors.New("wait done error")
+
 	wd := NewWaitDone()
 	go func() {
 		time.After(time.Millisecond)
@@ -29,4 +34,87 @@ func TestWaitDone_ReturnError(t *testing.T) {
 	if err == nil || !errors.Is(err, errWaitDone) {
 		t.Errorf("test wait done with error failed, expect:%s actual:%s", errWaitDone, err)
 	}
+}
+
+func TestWaitDone_AlreadyDone(t *testing.T) {
+	wd := NewWaitDone()
+	wd.Done()
+	err := wd.Wait()
+	if err != nil {
+		t.Errorf("test wait done error =>%s", err)
+	}
+}
+
+func TestWaitDone_WaitManyTimes(t *testing.T) {
+	wd := NewWaitDone()
+	wg := sync.WaitGroup{}
+	count := 10
+	wg.Add(count)
+	go func() {
+		time.After(time.Millisecond)
+		for i := 0; i < count; i++ {
+			go func() {
+				if err := wd.Wait(); err != nil {
+					t.Errorf("test wait done error =>%s", err)
+				}
+				wg.Done()
+			}()
+		}
+		wd.Done()
+	}()
+	err := wd.Wait()
+	if err != nil {
+		t.Errorf("test wait done error =>%s", err)
+	}
+	wg.Wait()
+}
+
+func TestWaitDone_DoneManyTimes(t *testing.T) {
+	wd := NewWaitDone()
+	wg := sync.WaitGroup{}
+	count := 10
+	wg.Add(count)
+	go func() {
+		time.After(time.Millisecond)
+		for i := 0; i < count; i++ {
+			wd.Done()
+			wg.Done()
+		}
+	}()
+	err := wd.Wait()
+	if err != nil {
+		t.Errorf("test wait done error =>%s", err)
+	}
+	wg.Wait()
+}
+
+func TestWaitDone_WaitAndDoneManyTimes(t *testing.T) {
+	wd := NewWaitDone()
+	wg := sync.WaitGroup{}
+	count := 100
+	wg.Add(count*2 + 1)
+	go func() {
+		time.After(time.Millisecond)
+		for i := 0; i < count; i++ {
+			go func() {
+				if err := wd.Wait(); err != nil {
+					t.Errorf("test wait done error =>%s", err)
+				}
+				wg.Done()
+			}()
+		}
+		wd.Done()
+		wg.Done()
+		for i := 0; i < count; i++ {
+			go func() {
+				wd.DoneWithError(errWaitDone)
+				wg.Done()
+			}()
+		}
+	}()
+	err := wd.Wait()
+	if err != nil {
+		t.Errorf("test wait done error =>%s", err)
+	}
+	wg.Wait()
 }
