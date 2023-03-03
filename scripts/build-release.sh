fef0654bb1796ac2aafc3435c56d6da738042177
mstmdev <mstmdev@gmail.com>
2023-03-04 01:54:46 +0800

    Update scripts/build-release.sh script, support to release macOS software (#107)

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -21,7 +21,7 @@ go build -v -o . ./...
 export GOFS_RELEASE_GO_VERSION=$(go version | awk '{print $3}')
 export GOFS_RELEASE_VERSION=$(./gofs -v | awk 'NR==1 {print $3}')
 
-# release path, for example, gofs_go1.18.1_amd64_linux_v0.4.0
+# release path, for example, gofs_go1.20.1_amd64_linux_v0.6.0
 export GOFS_RELEASE="gofs_${GOFS_RELEASE_GO_VERSION}_${GOARCH}_${GOOS}_${GOFS_RELEASE_VERSION}"
 
 rm -rf "$GOFS_RELEASE"
@@ -47,7 +47,7 @@ go build -v -o . ./...
 # build gofs with -ldflags="-H windowsgui" flag
 go build -v -ldflags="-H windowsgui" -o ./gofs_background.exe ./cmd/gofs
 
-# release path, for example, gofs_go1.18.1_amd64_windows_v0.4.0
+# release path, for example, gofs_go1.20.1_amd64_windows_v0.6.0
 export GOFS_RELEASE="gofs_${GOFS_RELEASE_GO_VERSION}_${GOARCH}_${GOOS}_${GOFS_RELEASE_VERSION}"
 
 mkdir "$GOFS_RELEASE"
@@ -59,6 +59,29 @@ zip -r "$GOFS_RELEASE.zip" "$GOFS_RELEASE"
 rm -rf "$GOFS_RELEASE"
 
 ############################# windows-release #############################
+
+############################## macOS-release ##############################
+
+# set go env for macOS
+export GOOS=darwin
+export GOARCH=amd64
+
+# build gofs
+go build -v -o . ./...
+
+# release path, for example, gofs_go1.20.1_amd64_darwin_v0.6.0
+export GOFS_RELEASE="gofs_${GOFS_RELEASE_GO_VERSION}_${GOARCH}_${GOOS}_${GOFS_RELEASE_VERSION}"
+
+rm -rf "$GOFS_RELEASE"
+mkdir "$GOFS_RELEASE"
+mv gofs "$GOFS_RELEASE/"
+
+# macOS release archive
+tar -zcvf "$GOFS_RELEASE.tar.gz" "$GOFS_RELEASE"
+
+rm -rf "$GOFS_RELEASE"
+
+############################## macOS-release ##############################
 
 # reset commit file
 echo -e "\c" >version/commit
