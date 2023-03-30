345f24401b2519f8181d1562f53bca406d46e667
mstmdev <mstmdev@gmail.com>
2023-03-30 13:38:02 +0800

    Remove the global variable in the conf package

--- server/handler/manage_handler.go
+++ server/handler/manage_handler.go
@@ -13,41 +13,34 @@ import (
 
 type manageHandler struct {
 	logger log.Logger
-	addr   string
+	conf   conf.Config
 }
 
 // NewManageHandlerFunc returns a gin.HandlerFunc that shows the application config
-func NewManageHandlerFunc(logger log.Logger, addr string) gin.HandlerFunc {
+func NewManageHandlerFunc(logger log.Logger, conf conf.Config) gin.HandlerFunc {
 	return (&manageHandler{
 		logger: logger,
-		addr:   addr,
+		conf:   conf,
 	}).Handle
 }
 
 func (h *manageHandler) Handle(c *gin.Context) {
 	format := strings.ToLower(c.Query(server.ParamFormat))
-	var result server.ApiResult
-	// copy the config and mask the user info for security
-	cp := conf.GetGlobalConfig(h.addr)
-	if cp == nil {
-		result = server.NewErrorApiResult(contract.NotFound, contract.NotFoundDesc)
-	} else {
-		config := *cp
-		mask := "******"
-		if len(config.Users) > 0 {
-			config.Users = mask
-		}
-		if len(config.SessionConnection) > 0 {
-			config.SessionConnection = mask
-		}
-		if len(config.EncryptSecret) > 0 {
-			config.EncryptSecret = mask
-		}
-		if len(config.DecryptSecret) > 0 {
-			config.DecryptSecret = mask
-		}
-		result = server.NewApiResult(contract.Success, contract.SuccessDesc, config)
+	config := h.conf
+	mask := "******"
+	if len(config.Users) > 0 {
+		config.Users = mask
 	}
+	if len(config.SessionConnection) > 0 {
+		config.SessionConnection = mask
+	}
+	if len(config.EncryptSecret) > 0 {
+		config.EncryptSecret = mask
+	}
+	if len(config.DecryptSecret) > 0 {
+		config.DecryptSecret = mask
+	}
+	result := server.NewApiResult(contract.Success, contract.SuccessDesc, config)
 	if format == conf.YamlFormat.Name() {
 		c.YAML(http.StatusOK, result)
 	} else {
