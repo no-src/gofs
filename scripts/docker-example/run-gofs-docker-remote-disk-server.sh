b6be8b15516bc6f7754f8384834ca9523a0b6569
mstmdev <mstmdev@gmail.com>
2023-04-19 06:41:49 +0800

    Fix docker example scripts for #146 (#149)

--- scripts/docker-example/run-gofs-docker-remote-disk-server.sh
+++ scripts/docker-example/run-gofs-docker-remote-disk-server.sh
@@ -11,4 +11,4 @@ docker run -it --rm -v "$PWD":"$WORKDIR" -w "$WORKDIR" --name go-generate-cert g
   go run /usr/local/go/src/crypto/tls/generate_cert.go --host $GOFS_SERVER_ADDR
 
 docker run -it --rm -v "$PWD":"$WORKDIR" -p 8105:8105 -p 443:443 --name running-gofs-remote-disk-server nosrc/gofs:latest \
-  gofs -source="rs://0.0.0.0:8105?mode=server&local_sync_disabled=true&path=$WORKDIR/remote-disk-server/source&fs_server=https://$GOFS_SERVER_ADDR" -dest="$WORKDIR/remote-disk-server/dest" -users="gofs|password|r" -tls_cert_file=cert.pem -tls_key_file=key.pem
+  gofs -source="rs://0.0.0.0:8105?mode=server&local_sync_disabled=true&path=$WORKDIR/remote-disk-server/source&fs_server=https://$GOFS_SERVER_ADDR" -dest="$WORKDIR/remote-disk-server/dest" -users="gofs|password|r" -tls_cert_file=cert.pem -tls_key_file=key.pem -token_secret=mysecret_16bytes
--- scripts/docker-example/run-gofs-docker-remote-push-client.sh
+++ scripts/docker-example/run-gofs-docker-remote-push-client.sh
@@ -8,4 +8,4 @@ export GOFS_SERVER_ADDR=10.0.4.8
 export WORKDIR=/workspace
 
 docker run -it --rm -v "$PWD":"$WORKDIR" --name running-gofs-remote-push-client nosrc/gofs:latest \
-  gofs -source="$WORKDIR/remote-push-client/source" -dest="rs://$GOFS_SERVER_ADDR:8105?local_sync_disabled=false&path=$WORKDIR/remote-push-client/dest" -users="gofs|password"
+  gofs -source="$WORKDIR/remote-push-client/source" -dest="rs://$GOFS_SERVER_ADDR:8105?local_sync_disabled=false&path=$WORKDIR/remote-push-client/dest" -users="gofs|password" -tls_cert_file=cert.pem
