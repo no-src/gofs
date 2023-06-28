2414fa3624936c5f76d2166ee04d3d37b743b54d
mstmdev <mstmdev@gmail.com>
2023-06-28 16:19:27 +0800

    Add comment

--- sync/sync.go
+++ sync/sync.go
@@ -46,6 +46,7 @@ func NewSync(opt Option) (Sync, error) {
 	s, err := newSync(opt)
 	if err == nil && opt.DryRun {
 		log.Info("dry run mode is enabled and no files will actually be written!")
+		// should we call the s.Close() here to close the old Sync?
 		s, err = NewEmptySync(opt)
 	}
 	return s, err
