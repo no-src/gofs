363da1c621353a492386dd2b6c5f5930097576b5
mstmdev <mstmdev@gmail.com>
2023-09-05 14:29:13 +0800

    Get last git commit from HEAD

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -7,7 +7,7 @@ git checkout main
 #git pull --no-rebase
 
 # update the last git commit
-echo -e "$(git rev-parse main)\c" >internal/version/commit
+echo -e "$(git rev-parse HEAD)\c" >internal/version/commit
 
 # set GOPROXY environment variable
 # export GOPROXY=https://goproxy.cn
