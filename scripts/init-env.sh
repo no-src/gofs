07d2fc771dfcabed5f1359d9b3093db0739fd75c
mstmdev <mstmdev@gmail.com>
2023-09-12 11:19:36 +0800

    Add sudo

--- scripts/init-env.sh
+++ scripts/init-env.sh
@@ -2,8 +2,8 @@
 
 # https://github.com/quic-go/quic-go/wiki/UDP-Buffer-Sizes
 if [ "$(uname -s)" == "Darwin" ]; then
-  sysctl -w kern.ipc.maxsockbuf=3014656
+  sudo sysctl -w kern.ipc.maxsockbuf=3014656
 else
-  sysctl -w net.core.rmem_max=2500000
-  sysctl -w net.core.wmem_max=2500000
+  sudo sysctl -w net.core.rmem_max=2500000
+  sudo sysctl -w net.core.wmem_max=2500000
 fi
