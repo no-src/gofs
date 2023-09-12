94e7f3241971d7e6f1efc35823fa8aa7b6a22c27
mstmdev <mstmdev@gmail.com>
2023-09-12 11:16:55 +0800

    Fix macOS

--- scripts/init-env.sh
+++ scripts/init-env.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 # https://github.com/quic-go/quic-go/wiki/UDP-Buffer-Sizes
-sysctl -w net.core.rmem_max=2500000
-sysctl -w net.core.wmem_max=2500000
+if [ "$(uname -s)" == "Darwin" ]; then
+  sysctl -w kern.ipc.maxsockbuf=3014656
+else
+  sysctl -w net.core.rmem_max=2500000
+  sysctl -w net.core.wmem_max=2500000
+fi
