c8d7ea8dba8b416bfe750ad0b908d2367bd54b80
mstmdev <mstmdev@gmail.com>
2023-10-30 16:34:28 +0800

    Trim prefix '/'

--- driver/minio/minio.go
+++ driver/minio/minio.go
@@ -286,7 +286,7 @@ func (c *minIODriver) GetFileTime(path string) (cTime time.Time, aTime time.Time
 
 func (c *minIODriver) WalkDir(root string, fn fs.WalkDirFunc) error {
 	return c.reconnectIfLost(func() error {
-		infoChan := c.client.ListObjects(c.ctx, c.bucketName, minio.ListObjectsOptions{Recursive: true, Prefix: root})
+		infoChan := c.client.ListObjects(c.ctx, c.bucketName, minio.ListObjectsOptions{Recursive: true, Prefix: strings.TrimPrefix(root, "/")})
 		for info := range infoChan {
 			if err := fn(info.Key, fs.FileInfoToDirEntry(newMinIOFileInfo(info)), info.Err); err != nil {
 				return err
