088bc3e0f1714343508c6ead4c4ec1f8a7a69fa1
mstmdev <mstmdev@gmail.com>
2024-02-06 17:12:12 +0800

    Update run-gofs-docker-timezone.sh

--- scripts/docker-example/run-gofs-docker-timezone.sh
+++ scripts/docker-example/run-gofs-docker-timezone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "default timezone:"
-docker run --rm -e nosrc/gofs:latest date
+docker run --rm nosrc/gofs:latest date
 echo "Asia/Shanghai timezone:"
 docker run --rm -e TZ=Asia/Shanghai nosrc/gofs:latest date
