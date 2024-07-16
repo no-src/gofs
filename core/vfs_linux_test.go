5b2ba87b273e96f83a395310216f2b37b332970e
mstmdev <mstmdev@gmail.com>
2024-07-16 17:49:53 +0800

    Fix test

--- core/vfs_linux_test.go
+++ core/vfs_linux_test.go
@@ -15,7 +15,7 @@ func TestVFS_SSHConfig(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			vfs := NewVFS(tc.path)
-			assert(t, vfs.Host() == "127.0.0.1", "compare Host error, expect:%s, actual:%s", "127.0.0.1", vfs.SSHConfig().Username)
+			assert(t, vfs.Host() == "127.0.0.1", "compare Host error, expect:%s, actual:%s", "127.0.0.1", vfs.Host())
 			assert(t, vfs.SSHConfig().Username == "hello", "compare Username error, expect:%s, actual:%s", "hello", vfs.SSHConfig().Username)
 			assert(t, vfs.SSHConfig().Password == "sftp_pwd", "compare Password error, expect:%s, actual:%s", "sftp_pwd", vfs.SSHConfig().Password)
 			assert(t, vfs.Port() == 8818, "compare Port error, expect:%d, actual:%d", 8818, vfs.Port())
