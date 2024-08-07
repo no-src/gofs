aee7e8776c29bad47a72313ed84d0b4107002aef
mstmdev <mstmdev@gmail.com>
2024-08-07 18:25:29 +0800

    Supports transport credentials with an insecure transport (#376)
    Supports transport credentials with an insecure transport

--- integration/integration_remote_test.go
+++ integration/integration_remote_test.go
@@ -15,6 +15,7 @@ func TestIntegration_RemoteDisk(t *testing.T) {
 	}{
 		{"gofs remote disk", "run-gofs-remote-disk-server.yaml", "run-gofs-remote-disk-client.yaml", "test-gofs-remote-disk.yaml"},
 		{"gofs remote disk with HTTP3", "run-gofs-remote-disk-server-with-http3.yaml", "run-gofs-remote-disk-client-with-http3.yaml", "test-gofs-remote-disk.yaml"},
+		{"gofs remote disk with insecure", "run-gofs-remote-disk-server-with-insecure.yaml", "run-gofs-remote-disk-client-with-insecure.yaml", "test-gofs-remote-disk.yaml"},
 		{"gofs remote push", "run-gofs-remote-push-server.yaml", "run-gofs-remote-push-client.yaml", "test-gofs-remote-push.yaml"},
 		{"gofs remote push with HTTP3", "run-gofs-remote-push-server-with-http3.yaml", "run-gofs-remote-push-client-with-http3.yaml", "test-gofs-remote-push.yaml"}}
 
